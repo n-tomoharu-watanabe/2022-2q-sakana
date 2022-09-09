3cc885cf7f133e5bb56cec8bd5b42b2ba5287ce5
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2022-09-09 10:08:24 +0900

    chakrauiを導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 import { App } from './app'
 
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 )
