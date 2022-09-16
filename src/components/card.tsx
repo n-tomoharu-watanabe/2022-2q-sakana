0e01d40ffc17f2b6f698a23b295d345e6faef2a5
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2022-09-16 10:03:15 +0900

    グラフの説明を追加

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,5 @@
-import { Box, BoxProps, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Image, Text } from "@chakra-ui/react"
+import { InfoIcon } from "@chakra-ui/icons"
+import { Box, BoxProps, Center, Collapse, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, Heading, IconButton, Image, Text } from "@chakra-ui/react"
 import { useState } from "react"
 
 type CardProps = BoxProps & {
@@ -7,6 +8,7 @@ type CardProps = BoxProps & {
 
 export const Card = ({ path, onClick, ...props }: CardProps) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  const [isGraphInfoOpen, setIsGraphInfoOpen] = useState(false)
 
   const fishName = path.match(/[^\/]+(?=\.png)/)?.toString()
 
@@ -62,7 +64,42 @@ export const Card = ({ path, onClick, ...props }: CardProps) => {
             </DrawerHeader>
             <DrawerCloseButton />
             <DrawerBody minH="64px">
-              {fishName}についての説明です。
+              <Text mb="4">
+                {fishName}についての説明です。
+              </Text>
+              <Flex mb="2" justify="space-between" align="center">
+                <Heading as="h4" size="md">
+                  グラフ
+                </Heading>
+                <IconButton
+                  variant="solid"
+                  rounded="full"
+                  size="xs"
+                  bg="gray.700"
+                  color="white"
+                  fontSize="20"
+                  aria-label='open graph infomation'
+                  icon={<InfoIcon />}
+                  onClick={() => {
+                    setIsGraphInfoOpen(bool => !bool)
+                  }}
+                />
+              </Flex>
+              <Collapse in={isGraphInfoOpen} animateOpacity>
+                <Text mb="4">
+                  グラフについての説明です。
+                </Text>
+              </Collapse>
+              <Center
+                h="64px"
+                bg="gray.500"
+                m="1"
+                color="white"
+                fontSize="xl"
+                rounded="base"
+              >
+                グラフ画像
+              </Center>
             </DrawerBody>
           </Box>
         </DrawerContent>
