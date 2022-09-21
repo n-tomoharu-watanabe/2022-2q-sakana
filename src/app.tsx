b7ed21931458453e2d5bc0184d9117045790fe32
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2022-09-21 16:44:37 +0900

    画像のpathを修正

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,20 @@ import { Box } from "@chakra-ui/react"
 import { FishGrid } from "./components/grid"
 import { Title } from "./components/title"
 
-const fishes = import.meta.glob('../public/images/fishes/*.jpg')
+const fishes = import.meta.glob(
+  '../public/images/fishes/*.jpg'
+)
+
+const fishPaths = Object.entries(fishes).map(([path, image]) => (
+  path.replace("../public", "")
+))
 
 export const App = () => {
   return (
     <Box bg="lightgray">
       <Box minHeight="100vh" maxWidth="md" margin="0 auto" bg="white">
         <Title />
-        <FishGrid paths={Object.entries(fishes).map(([path, image]) => path)} />
+        <FishGrid paths={fishPaths} />
       </Box>
     </Box>
   )
