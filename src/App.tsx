98a489904063d5c08b308169fb9663481479606d
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2022-09-21 10:09:31 +0900

    Appを仮作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Box } from "@chakra-ui/react"
+import { Card } from "./components/card"
+import { Title } from "./components/title"
+
+const fishes = import.meta.glob('../public/images/fishes/*.png')
+
+function range(length: number) {
+  return Array.from({ length }, (_, i) => i)
+}
+
 export const App = () => {
   return (
-    <h1>This is App</h1>
+    <Box bg="lightgray">
+      <Box minHeight="100vh" maxWidth="md" margin="0 auto" bg="white">
+        <Title />
+        {Object.entries(fishes).map(([path, image]) => (
+          range(30).map((i) => (
+            <Card path={path} onClick={() => {
+            }} key={`${path}-${i}`} />
+          ))
+        ))}
+      </Box>
+    </Box>
   )
 }
