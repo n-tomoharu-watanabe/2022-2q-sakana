f4a238c346b29c381506228c4208e7a0b55f971e
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2022-09-25 18:18:37 +0900

    react-queryとaxiosをセットアップ

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+const queryClient = new QueryClient()
+
 import { App } from './app'
 
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  </QueryClientProvider>
 )
